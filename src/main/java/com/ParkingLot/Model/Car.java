08a35f05d1635f021132e906d35f1bb5a15ea6c0
Abhishek2.anand <abhishek2.anand@paytmmall.com>
2020-07-31 19:53:39 +0530

    Vehicle class Changes

--- src/main/java/com/ParkingLot/Model/Car.java
+++ src/main/java/com/ParkingLot/Model/Car.java
@@ -1,25 +1,6 @@
 package com.ParkingLot.Model;
 
 public class Car extends Vehicle{
-    String regNo;
-    String color;
-
-
-    public String getRegNo() {
-        return regNo;
-    }
-
-    public void setRegNo(String regNo) {
-        this.regNo = regNo;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
 
     public Car(String regNo, String color) {
         this.regNo = regNo;
--- src/main/java/com/ParkingLot/Model/Vehicle.java
+++ src/main/java/com/ParkingLot/Model/Vehicle.java
@@ -5,12 +5,20 @@ public abstract class Vehicle {
     String regNo;
     String color;
 
-    public abstract String getRegNo();
+    public String getRegNo() {
+        return regNo;
+    }
 
-    public abstract void setRegNo(String regNo);
+    public void setRegNo(String regNo) {
+        this.regNo = regNo;
+    }
 
-    public abstract String getColor();
+    public String getColor() {
+        return color;
+    }
 
-    public abstract void setColor(String color);
+    public void setColor(String color) {
+        this.color = color;
+    }
 
 }
